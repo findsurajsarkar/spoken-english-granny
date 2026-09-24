c2d3ff79031862ae90fab9c5419720791f0de8d4
findsurajsarkar <145889403+findsurajsarkar@users.noreply.github.com>
2026-09-24 10:26:48 +0530

    Plus code: creative_fish_51930 (lifetime)

--- src/plusCodes.ts
+++ src/plusCodes.ts
@@ -5,4 +5,5 @@
 export const PLUS_CODES: Array<{ h: string; plan: 'monthly' | 'lifetime'; until: string }> = [
   { h: '65ff7631b4411be08e63bb89d05c073e9a55a5752c34cd00a58e56ebc08e2a5d', plan: 'monthly', until: '2026-10-25T03:58:52.973Z' }, // creative_fish_51930 2026-09-24
   { h: '6e366c99d3fc3ab2ef279022e7f38eab5392ddaad9337b7c0e504d441aada6b2', plan: 'lifetime', until: '2999-09-24T04:49:08.133Z' }, // bold_bicycle_807768 2026-09-24
+  { h: '78e407da853e1609d104e2f4de0dff891a05075da64149a6e1bf904b9bb695c4', plan: 'lifetime', until: '2999-09-24T04:56:48.356Z' }, // creative_fish_51930 2026-09-24
 ];
